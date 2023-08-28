a332d9c6464a466c1e657cf79869297c2580342b
Eduardo Schmitt <zeduhhh@gmail.com>
2023-08-28 08:55:34 -0300

    att

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" id="${pokemon.number}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -19,18 +19,29 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            <img class="symbol" src="assets/img/symbol.png" alt="Pokebola" class="pokebola">
         </li>
     `
 }
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+        const newHtml = pokemons.map(convertPokemonToLi).join('');
+        pokemonList.innerHTML += newHtml;
+
+        const pokemonElements = document.querySelectorAll('.pokemon');
+        pokemonElements.forEach((pokemonElement) => {
+            pokemonElement.addEventListener('click', () => {
+                // Lógica a ser executada ao clicar em um Pokémon
+                // Por exemplo, exibir detalhes ou redirecionar para uma página de detalhes
+                console.log(`Clicou no Pokémon: ${pokemonElement.id}`);
+                console.log(pokeApi.getPokemonsSeparately(pokemonElement.id));
+            });
+        });
+    });
 }
 
-loadPokemonItens(offset, limit)
+loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -33,3 +33,78 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
+
+
+/* Separar */
+function convertSeparately(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    const abilitys = pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name)
+    const [ability] = abilitys
+
+    pokemon.abilitys = abilitys
+    pokemon.ability = ability
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokeApi.createModal(pokemon)
+}
+
+pokeApi.getPokemonSeparately = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemonsSeparately = (pokemonId) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => [convertSeparately(jsonBody)])
+        .then((pokemonsDetails) => pokemonsDetails)
+}
+
+// Adicione um evento de clique ao elemento de fechamento (x)
+document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('close')) {
+        const modal = document.getElementById('modal');
+        if (modal) {
+            modal.remove(); // Remove o modal do DOM
+        }
+    }
+});
+
+pokeApi.createModal = (pokemon) => {
+    console.log('criando...')
+    const content = document.getElementById('base-modal');
+    content.innerHTML += `
+        <div class="modal" id="modal">
+            <div class="modal-content ${pokemon.type}">
+                <span class="close">&times;</span>
+                <div class="modal-body ${pokemon.type}">
+                    <div class="modal-body-left">
+                        <img class="modal-img" src="${pokemon.photo}" alt="pokemon">
+                    </div>
+                    <div class="modal-body-right">
+                        <h1 class="modal-title">${pokemon.name}</h1>
+                    </div>
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <ol class="types">
+                        ${pokemon.abilitys.map((ability) => `<li class="ability ${ability}">${ability}</li>`).join('')}
+                    </ol>
+                </div>
+            </div>
+        </div>
+    `;
+}
